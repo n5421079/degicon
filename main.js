dd0f46ffcf70399e7c815ac47695c25348ad4884
n5421079 <114901341+n5421079@users.noreply.github.com>
2024-01-19 12:04:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// import list from "./questions/list.json" assert { type: "json" };
-// console.log(list);
+import list from "./questions/list.json" assert { type: "json" };
+console.log(list);
 
-var radioNodeList = document.option.genre;
-alert( radioNodeList.value );
+//var radioNodeList = document.option.genre;
+//alert( radioNodeList.value );
